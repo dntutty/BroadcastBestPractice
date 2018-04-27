eb15a2c7f104d5f60159495fdbd2e60e7a9e8de1
Liusuixin <liusuixn@readtv.cn>
2018-04-27 16:17:33 +0800

    First commit

---
+++ app/src/main/java/com/example/a14669/broadcastbestpractice/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.a14669.broadcastbestpractice;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends BaseActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button forceOffline = (Button)findViewById(R.id.force_offline);
+        forceOffline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("com.example.a14669.broadcastbestpractice.FORCE_OFFLINE");
+                Log.d("he","MainActivity onClick");
+                sendBroadcast(intent);
+            }
+        });
+    }
+}
